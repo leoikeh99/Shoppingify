ba1b6efe515d8ac9bd1fb76327f56a0db245f090
Ikeh Leonard <leoikeh99@gmail.com>
2021-03-26 13:10:40 +0100

    ready to deploy

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const db = require("./config/db");
 
 const app = express();
 
-app.use(express.json({ extended: false }));
 db();
+app.use(express.json({ extended: false }));
 
 //routes set-up
 app.use("/api/auth", require("./routes/auth"));
@@ -12,6 +12,13 @@ app.use("/api/user", require("./routes/user"));
 app.use("/api/items", require("./routes/items"));
 app.use("/api/cart", require("./routes/cart"));
 app.use("/api/history", require("./routes/history"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
